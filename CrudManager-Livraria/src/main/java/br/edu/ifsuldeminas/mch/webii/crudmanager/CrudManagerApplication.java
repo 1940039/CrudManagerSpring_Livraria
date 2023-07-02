75be2b94c126368c2cac2e1898dd3b58b64e1874
1940039 <107783538+1940039@users.noreply.github.com>
2023-07-02 06:53:50 -0300

    Add files via upload

---
+++ CrudManager-Livraria/src/main/java/br/edu/ifsuldeminas/mch/webii/crudmanager/CrudManagerApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifsuldeminas.mch.webii.crudmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudManagerApplication.class, args);
+	}
+}
